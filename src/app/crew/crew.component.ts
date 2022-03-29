5570cf7423a9cddaa92c36a9392a1bb55b7cf909
EnrZ <ez_25_25@yahoo.com>
2022-03-28 21:33:48 -0500

    MVP commit

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -9,6 +9,8 @@ export class CrewComponent implements OnInit {
 
   inCrew: boolean = false;
   crew: object[] = [];
+  //for setmemberhovered
+  hoveredCandidate: object; //undefined by default
 
   candidates: object[] = [
     {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
@@ -25,5 +27,43 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+
+  addCrewMember(candidate: object){
+    //check if candidate is part of the crew
+    for(let i=0;i<this.crew.length;i++){
+      if (this.crew[i]["name"] === candidate["name"]){
+
+        //candidate exists in the crew array - doing this
+        //even though kyle doesnt reccomend manipulating arrays inside of for loop unless returning right afterwards which is the case here
+        this.crew.splice(i,1);
+        return;
+      }
+    }
+    //if the crew size less than 3
+    if (this.crew.length >= 3){
+      return;
+    }
+
+
+    //then their data should be added to crew array
+    this.crew.push(candidate);
+  }
+
+
+  isCandidateSelected(candidate: object): boolean{
+    for(let i=0;i<this.crew.length;i++){
+      if (this.crew[i]["name"] === candidate["name"]){
+        return true;
+      }
+
+    }
+    return false;
+  }
+
+  setMemberHovered(candidate: object){
+    this.hoveredCandidate = candidate;
+  }
+
+
 
 }
